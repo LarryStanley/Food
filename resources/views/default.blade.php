df03175b830829e97effd2446ac24951bce0b0a1
Stanley <kill.xmurderer@gmail.com>
2015-07-30 10:55:08 -0400

    Update.

---
+++ resources/views/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>中大美食(其實沒有)</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.3.0/css/material-fullpalette.css">
+	<link rel="stylesheet" href="/css/main.css">
+
+	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+</head>
+<body>
+	@yield("content")
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends("default")
+
+@section("content")
+<div class="center" id="search">
+	<h1  style="display:inline">中大美食</h1>(其實沒有)
+	<div class="form-group-material-grey-400">
+		<input type="text" class="form-control" placeholder="立即查詢餐廳 例如：樂活堡" style="color: white">
+		<button class="btn btn-default pull-right" style="color: white">查詢</button>
+	</div>
+</div>
+@stop
