6a6bb331708b6a7a96e562387c7f4113dd7cbde0
LarryStanley <kill.xmurderer@gmail.com>
2015-08-02 13:38:44 +0800

    Handle 404 not found.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends("default")
+
+@section("content")
+<div class="container">
+	<div class="center">
+		<div id="title">
+			<a href="/"><h1  style="display:inline">中大美食</h1> beta (其實沒有)</a>
+		</div>
+		<div class="well" id='search' style="overflow: auto;">
+			<h1>404 找不到頁面！！</h1>
+			<hr>
+			<p>真難過，找不到你想要找的頁面</p>
+			<p>找不到你知道的餐廳？<a href="/add-food" class="btn btn-flat btn-info" >新增餐廳</a></p>
+		</div>
+	</div>
+</div>
+@stop
