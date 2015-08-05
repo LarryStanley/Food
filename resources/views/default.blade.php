dc5a86d925cb54f7d796885f08fff6890dede14f
LarryStanley <kill.xmurderer@gmail.com>
2015-08-05 22:37:28 +0800

    Minify js

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -37,7 +37,7 @@
 	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 	<script src="/js/jquery.autocomplete.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-	<script src="/js/main.js"></script>
+	<script src="/js/main.min.js"></script>
 
 </head>
 <body>
