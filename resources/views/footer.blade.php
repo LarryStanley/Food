dd29719dc3cf3278ba6918ecc31f71d3758814a3
LarryStanley <kill.xmurderer@gmail.com>
2015-09-04 21:03:51 +0800

    Update footer.

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,7 +2,12 @@
 	<div class="row">
 		<div class="col-md-6 col-sm-6">
 			找不到你知道的餐廳？
-			<a href="/add-food" class="btn btn-default" style="color: white;">立即新增</a>	
+			<?php
+				if (Session::get("facebookId"))
+					echo '<a href="/add-food" class="btn btn-default" style="color: white;">立即新增</a>';
+				else
+					echo '<a href="/add-food" class="btn btn-default" style="color: white;">登入新增</a>';
+			?>
 		</div>
 		<div class="col-md-6 col-sm-6" id="feedbackButtons">
 			<a href="/feedback" class="btn btn-default" style="color: white">回報錯誤</a>
